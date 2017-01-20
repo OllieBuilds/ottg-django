13ec4d451f777f4b2d5de44f99e61b749becc278
OllieBuilds <scarborough.oliver@gmail.com>
2017-01-20 14:28:12 -0500

    Creates new URL, view, & template to display lists
    Refactors and separates Home Page and List views

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get( self.live_server_url )
